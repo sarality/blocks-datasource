8d6f723a2cc3ccc3912b5f685fa7931232e26ba0
Satya <spuniani@users.noreply.github.com>
2016-11-18 16:47:54 +0530

    composite data source definition (#9)
    * composite data source definition
    
    * CL review comments

---
+++ src/main/java/com/sarality/datasource/CompositeDataSource.java
@@ -0,0 +1,91 @@
+package com.sarality.datasource;
+
+import com.sarality.db.common.FieldValueGetter;
+import com.sarality.db.common.ChildDataSetter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Data Source that composes a data object by combining data from multiple data sources.
+ *
+ * @author satya@ (Satya Puniani)
+ */
+public class CompositeDataSource<T, A, I> implements DataSource<List<T>> {
+
+  private final DataSource<List<T>> dataSource;
+
+  private DataSource<List<A>> associatedSource;
+
+  private FieldValueGetter<T, I> parentIndexExtractor;
+  private FieldValueGetter<A, I> childIndexExtractor;
+  private ChildDataSetter<T, A> parentFieldValueSetter;
+
+  private List<T> dataList;
+  private Map<I, List<T>> indexedDataMap = new HashMap<>();
+
+  public CompositeDataSource(DataSource<List<T>> dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  public CompositeDataSource<T, A, I> composeWith(DataSource<List<A>> associatedSource,
+      FieldValueGetter<T, I> parentIndexExtractor, FieldValueGetter<A, I> associatedDataIndexExtractor,
+      ChildDataSetter<T, A> setter) {
+
+    this.associatedSource = associatedSource;
+    this.parentIndexExtractor = parentIndexExtractor;
+    this.childIndexExtractor = associatedDataIndexExtractor;
+    this.parentFieldValueSetter = setter;
+
+    return this;
+  }
+
+  @Override
+  public List<T> load() {
+    dataList = dataSource.load();
+    I index;
+    List<T> values;
+
+    for (T data : dataList) {
+      index = parentIndexExtractor.getValue(data);
+      values = indexedDataMap.get(index);
+      if (values == null) {
+        values = new ArrayList<T>();
+        indexedDataMap.put(index, values);
+      }
+      values.add(data);
+    }
+
+    setAssociatedData();
+
+    return dataList;
+  }
+
+  @Override
+  public List<T> getData() {
+    return dataList;
+  }
+
+  private void setAssociatedData() {
+
+    if (associatedSource == null) {
+      return;
+    }
+
+    I index;
+    List<T> values;
+    List<A> associatedDataList = associatedSource.load();
+
+    for (A associatedData : associatedDataList) {
+      index = childIndexExtractor.getValue(associatedData);
+      values = indexedDataMap.get(index);
+      if (values != null) {
+        for (T parentData : values) {
+          parentFieldValueSetter.setChildData(parentData, associatedData);
+        }
+      }
+    }
+  }
+}
